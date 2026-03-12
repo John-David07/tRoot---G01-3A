dd73322d73c7d74eec8d314925b0107847ddc8de
David <erhmaster@gmail.com>
2026-03-12 23:32:22 +0800

    Updated main.dart in accordance to recent changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:plant_monitoring_system/screens/dashboard_screen.dart';
+import 'package:plant_monitoring_system/utils/theme_manager.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -11,18 +12,28 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = true; // Default to dark mode as per your design
+
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Plant Monitor',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        useMaterial3: true,
-      ),
-      home: const DashboardScreen(),
+      theme: _isDarkMode ? ThemeManager.darkTheme : ThemeManager.lightTheme,
+      home: DashboardScreen(),
     );
   }
 }
